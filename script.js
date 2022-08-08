d1f60740f1131489e0a065beacaea1968834288a
nokamaikato2001 <104317589+nokamaikato2001@users.noreply.github.com>
2022-08-09 01:42:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,162 @@
+var audio = new Audio('assets/sentmessage.mp3');
+var contactString = "<div class='social'> <a target='_blank' href='tel:+918178498674'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>8178498674</label></div> </a> <a href='mailto:npawan221@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/nokamaikato2001'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://www.instagram.com/pawannegi__/'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/pawan-negi-334a0a19b/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.8210425513903!2d77.01612431482202!3d28.575136082440416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1a9bd009f789%3A0xd35d635b64a14e39!2sQutub%20Vihar%20II%2C%20Qutub%20Vihar%20Phase%202%2C%20Goyla%20Village%2C%20Delhi%2C%20110071!5e0!3m2!1sen!2sin!4v1659986327857!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>Dwarka Sector19, Delhi, INDIA 110071</address>";
+function startFunction() {
+    setLastSeen();
+    waitAndResponce("intro");
+}
+
+function setLastSeen() {
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
+}
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+function sendMsg() {
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "sent");
+    greendiv.setAttribute("class", "green");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = input.value;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1500);
+    input.value = "";
+    playSound();
+}
+
+function waitAndResponce(inputText) {
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "typing...";
+    switch (inputText.toLowerCase().trim()) {
+        case "intro":
+            setTimeout(() => {
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Pawan Singh Negi</a>.</span><br><br>I am a BCA student at <span class='bold'>IGNOU 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job/Internship openings in the software engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+            }, 2000);
+            break;
+        case "help":
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            break;
+        case "skills":
+            sendTextMessage("<span class='sk'>I am currently pursuing BCA from IGNOU.<br><br>I can write and understand programming language. Also i love to learn new things.<br>Favourite IDE:VSCode</span>");
+            break;
+        case "address":
+            sendTextMessage(addressString);
+            break;
+        case "clear":
+            clearChat();
+            break;
+        case "about":
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by me(Pawan Singh Negi). with ❤️</span>");
+            break;
+        case "contact":
+            sendTextMessage(contactString);
+            break;
+        case "new":
+            sendTextMessage(addressString);
+            break;
+        default:
+            setTimeout(() => {
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' to know more about usage.");
+            }, 2000);
+            break;
+    }
+
+
+
+}
+
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
+
+
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = "#";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function playSound() {
+    audio.play();
+}
